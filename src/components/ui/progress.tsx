d7cc555acbbf4b0fc78e04bf8b14db598d1145bf
realfreeclub-cloud <realfreeclub@gmail.com>
2026-04-24 05:28:12 +0530

    fix

---
+++ src/components/ui/progress.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import * as React from "react"
+import { cn } from "@/lib/utils"
+
+const Progress = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement> & { value?: number }
+>(({ className, value, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn(
+      "relative h-4 w-full overflow-hidden rounded-full bg-slate-100",
+      className
+    )}
+    {...props}
+  >
+    <div
+      className="h-full w-full flex-1 bg-blue-600 transition-all duration-300 ease-in-out"
+      style={{ width: `${value || 0}%` }}
+    />
+  </div>
+))
+Progress.displayName = "Progress"
+
+export { Progress }
