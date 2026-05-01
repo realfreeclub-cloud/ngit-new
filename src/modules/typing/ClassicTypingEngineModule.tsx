f6736e7436fcf2b7387438958f7564486ecd831e
realfreeclub-cloud <realfreeclub@gmail.com>
2026-05-01 19:52:10 +0530

    fix

--- src/modules/typing/ClassicTypingEngineModule.tsx
+++ src/modules/typing/ClassicTypingEngineModule.tsx
@@ -487,9 +487,19 @@ export const ClassicTypingEngineModule: React.FC<ClassicTypingEngineModuleProps>
             Submit
           </button>
           
-          <div className="absolute right-0 w-8 h-8 bg-[#9b59b6] rounded-full flex items-center justify-center cursor-pointer text-white hover:bg-[#8e44ad] transition-colors">
-            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeWidth="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"></path><path strokeLinecap="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path></svg>
-          </div>
+          <button 
+            onClick={() => {
+              if (confirm("Are you sure you want to reset the test? Current progress will be lost.")) {
+                resetTest();
+              }
+            }}
+            className="absolute right-0 w-8 h-8 bg-[#9b59b6] rounded-full flex items-center justify-center cursor-pointer text-white hover:bg-[#8e44ad] transition-colors shadow-lg"
+            title="Reset Test"
+          >
+            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path>
+            </svg>
+          </button>
         </div>
       </div>
     </div>
